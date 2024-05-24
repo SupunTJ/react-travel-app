38cfe540e321d1ff52b333e13810088f241bdc0e
SupunTJ <jayaweeraspn@gmail.com>
2024-05-24 19:24:21 +0530

    implementing update functionalty

--- src/components/DestinationList.jsx
+++ src/components/DestinationList.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useGetAllDestinationsQuery } from "../api/destinationApi";
-import { useDeleteDestinationMutation } from "../api/destinationApi";
+import Destination from "./Destination";
 
 function DestinationList() {
   const { data, isLoading, isSuccess, isError, error } =
     useGetAllDestinationsQuery();
-
-  const [deleteDestinationMutation] = useDeleteDestinationMutation();
 
   //   const handleDelete = (e) => {
   //     e.preventDefault();
@@ -19,31 +17,7 @@ function DestinationList() {
   } else if (isSuccess) {
     // console.log(data);
     content = data.map((destination) => {
-      return (
-        <div
-          className="row py-1"
-          key={destination.id}
-          style={{
-            borderBottom: "1px solid #333",
-            borderTop: "1px solid #333",
-          }}
-        >
-          <div className="col-3 offset-3">
-            {destination.city}, {destination.country}
-          </div>
-          <div className="col-1 text-warning">
-            {destination.daysNeeded} days
-          </div>
-          <div className="col-2">
-            <button
-              className="btn btn-danger form-control"
-              onClick={() => deleteDestinationMutation({ id: destination.id })}
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      );
+      return <Destination destination={destination} key={destination.id} />;
     });
   } else if (isError) {
     content = <p>{error}</p>;
