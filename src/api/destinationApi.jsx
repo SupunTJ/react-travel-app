764778468bcab02b7ed2e5e56227fa1da2dbead2
SupunTJ <jayaweeraspn@gmail.com>
2024-05-24 17:34:38 +0530

    adding tags

--- src/api/destinationApi.jsx
+++ src/api/destinationApi.jsx
@@ -3,13 +3,48 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const destinationAPI = createApi({
   reducerPath: "apidestination",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
+  tagTypes: ["Destinations"],
   endpoints: (builder) => ({
     // QUERY -> GET
     //MUTATIONS -> POST/PUT/DELETE
     getAllDestinations: builder.query({
       query: () => "destination",
+      providesTags: ["Destinations"],
+    }),
+    // getAllDestinations: builder.query({
+    //     query: () => "destination",
+    //     providesTags: ["Destinations"],
+    //   }),
+    addDestination: builder.mutation({
+      query: (destination) => ({
+        url: "destination",
+        method: "POST",
+        body: destination,
+      }),
+      invalidatesTags: ["Destinations"],
+    }),
+    updateDestination: builder.mutation({
+      query: (destination) => ({
+        url: `destination/${destination.id}`,
+        method: "PUT",
+        body: destination,
+      }),
+      invalidatesTags: ["Destinations"],
+    }),
+    deleteDestination: builder.mutation({
+      query: (id) => ({
+        url: `destination/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+      invalidatesTags: ["Destinations"],
     }),
   }),
 });
 
-export const { useGetAllDestinationsQuery } = destinationAPI;
+export const {
+  useGetAllDestinationsQuery,
+  useAddDestinationMutation,
+  useUpdateDestinationMutation,
+  useDeleteDestinationMutation,
+} = destinationAPI;
--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import { useAddDestinationMutation } from "../api/destinationApi";
 
 function AddDestination() {
+  const [newCity, setNewCity] = useState("");
+  const [newCountry, setNewCountry] = useState("");
+  const [addDestinationMutation, results] = useAddDestinationMutation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addDestinationMutation({
+      id: Math.random() * 100,
+      city: newCity,
+      country: newCountry,
+      daysNeeded: parseInt(Math.random() * 10) + 1,
+    });
+    setNewCity("");
+    setNewCountry("");
+  };
+
   return (
     <div className="p-4 border">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row col-8 offset-2">
           <h4>Enter a new Destination</h4>
           <div className="col-5 p-1"></div>
@@ -11,12 +29,16 @@ function AddDestination() {
             type="text"
             className="form-control"
             placeholder="Enter City..."
+            value={newCity}
+            onChange={(e) => setNewCity(e.target.value)}
           />
           <div className="col-5 p-1"></div>
           <input
             type="text"
             className="form-control"
             placeholder="Enter Country..."
+            value={newCountry}
+            onChange={(e) => setNewCountry(e.target.value)}
           />
           <div className="col-2 p-1"></div>
           <button className="btn btn-success form-control">Add</button>
